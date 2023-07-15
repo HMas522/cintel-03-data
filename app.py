9e52b551b6bce5877cc792d9732fd12b2064bf2e
Hayley Massey <Hmassey12@outlook.com>
2023-07-15 14:24:51 -0500

    updated theme

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ from util_logger import setup_logger
 logger, logname = setup_logger(__name__)
 
 app_ui = ui.page_navbar(
-    shinyswatch.theme.cosmo(),
+    shinyswatch.theme.superhero(),
     ui.nav(
         "Home",
         ui.layout_sidebar(
