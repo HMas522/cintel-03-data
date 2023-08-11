d6b104e3371f6156a8134d9f989241959082a68c
Hayley Massey <Hmassey12@outlook.com>
2023-08-11 18:37:46 -0500

    updated app data once more

--- data/app_data.py
+++ data/app_data.py
@@ -33,7 +33,7 @@ penguins_df = sns.load_dataset("penguins")
 penguins_df.to_excel(data_folder.joinpath("penguins.xlsx"))
 penguins_df.to_csv(data_folder.joinpath("penguins.csv"))
 
-tips_df = pd.read_excel(r'C:/Users/Hayley/Downloads/tips.csv')
+tips_df = pd.read_excel("tips.csv")
 tips_df.to_excel(data_folder.joinpath("tips.csv"))
 
 mtcars_df = pd.read_csv(data_folder.joinpath("mtcars.csv"))
