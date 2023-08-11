22aeafbac0064487e0cb951a741a58cb49e3c488
Hayley Massey <Hmassey12@outlook.com>
2023-08-10 22:00:19 -0500

    using different csv pull method

--- data/app_data.py
+++ data/app_data.py
@@ -33,9 +33,8 @@ penguins_df = sns.load_dataset("penguins")
 penguins_df.to_excel(data_folder.joinpath("penguins.xlsx"))
 penguins_df.to_csv(data_folder.joinpath("penguins.csv"))
 
-tips_df = sns.load_dataset("tips")
+tips_df = pd.read_excel(r'C:/Users/Hayley/cintel-03-data/data/tips.xlsx')
 tips_df.to_excel(data_folder.joinpath("tips.xlsx"))
-tips_df.to_csv(data_folder.joinpath("tips.csv"))
 
 mtcars_df = pd.read_csv(data_folder.joinpath("mtcars.csv"))
 mtcars_df.to_excel(data_folder.joinpath("mtcars.xlsx"))
