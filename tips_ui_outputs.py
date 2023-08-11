539f412942ee4bc78d81419bf19f18b7605514ff
Hayley Massey <Hmassey12@outlook.com>
2023-08-10 22:18:19 -0500

    found typo

--- tips_ui_outputs.py
+++ tips_ui_outputs.py
@@ -7,7 +7,7 @@ Provide the exact name of the server function that will provide the output.
 from shiny import ui
 
 
-def get_mtcars_outputs():
+def get_tips_outputs():
     return ui.panel_main(
         ui.h2("Main Panel with Output (Not Yet Reactive)"),
         ui.tags.hr(),
